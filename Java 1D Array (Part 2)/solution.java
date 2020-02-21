5c85800ee592a6aa5b9da734154a06d60c73f572
phanindra-kakara <61313188+phanindra-kakara@users.noreply.github.com>
2020-02-21 16:12:17 +0530

    Create solution.java

---
+++ Java 1D Array (Part 2)/solution.java
@@ -0,0 +1,22 @@
+public static boolean canWin(int leap, int[] game) {
+	if (game == null) {
+		return false;
+	}
+    return isSolvable(leap, game, 0);
+}
+
+private static boolean isSolvable(int leap, int[] game, int i) {
+    // Base Cases
+    if (i >= game.length) {
+        return true;
+    } else if (i < 0 || game[i] == 1) {
+        return false;
+    }
+    
+    game[i] = 1; // marks as visited
+
+    // Recursive Cases (Tries +m first to try to finish game quickly)
+    return isSolvable(leap, game, i + leap)
+        || isSolvable(leap, game, i + 1)
+        || isSolvable(leap, game, i - 1);
+}
